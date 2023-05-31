2a447aa9fcffdc8744cdbee24d6d5a752062720d
Itamar <itamarshmaya@gmail.com>
2023-05-31 19:28:12 +0300

    added side menu to edit profile page

--- src/screens/editProfilePage/EditProfilePage/EditProfilePage.jsx
+++ src/screens/editProfilePage/EditProfilePage/EditProfilePage.jsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import AvatarEditForm from "./AvatarEditForm/AvatarEditForm";
 import DeleteUserButton from "./AvatarEditForm/DeleteUserButton/DeleteUserButton";
 import DeleteAccountConfirmWindow from "./AvatarEditForm/DeleteUserButton/DeleteAccountConfirmWindow/DeleteAccountConfirmWindow";
+import SideMenu from "../../profile/SideMenu/SideMenu";
 // import {
 // updateProfileData,
 // } from "../../../apis/animexpo/animexpo_updates";
@@ -62,64 +63,67 @@ const EditProfilePage = () => {
   // };
 
   return (
-    <div className="editprofile-page">
-      {openConfirmWindow && (
-        <DeleteAccountConfirmWindow
-          openConfirmWindow={openConfirmWindow}
-          setOpenConfirmWindow={setOpenConfirmWindow}
-        />
-      )}
-      <main className="edit-profile-content">
-        <h2>Edit</h2>
-        <AvatarEditForm />
-        <DeleteUserButton
-          openConfirmWindow={openConfirmWindow}
-          setOpenConfirmWindow={setOpenConfirmWindow}
-        />
-        {/* <form
+    <>
+      <SideMenu username={username} />
+      <div className="editprofile-page">
+        {openConfirmWindow && (
+          <DeleteAccountConfirmWindow
+            openConfirmWindow={openConfirmWindow}
+            setOpenConfirmWindow={setOpenConfirmWindow}
+          />
+        )}
+        <main className="edit-profile-content">
+          <h2>Edit</h2>
+          <AvatarEditForm />
+          <DeleteUserButton
+            openConfirmWindow={openConfirmWindow}
+            setOpenConfirmWindow={setOpenConfirmWindow}
+          />
+          {/* <form
           onSubmit={onEditFormSubmit}
           className="edit-profile-form"
           ref={formRef}
-        >
+          >
           <div className="edit-input">
-            <label htmlFor="gender">Gender</label>
-            <select
-              id="gender"
-              name="gender"
-              value={genderInput}
-              onChange={onChange}
-            >
-              <option value=""></option>
+          <label htmlFor="gender">Gender</label>
+          <select
+          id="gender"
+          name="gender"
+          value={genderInput}
+          onChange={onChange}
+          >
+          <option value=""></option>
               <option value="Other">Other</option>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
-            </select>
-          </div>
-          <div className="edit-input">
-            <label htmlFor="birthday">Birthday</label>
-            <input
+              </select>
+              </div>
+              <div className="edit-input">
+              <label htmlFor="birthday">Birthday</label>
+              <input
               id="birthday"
               type="date"
               value={birthdayInput}
               onChange={onChange}
               max={new Date().toISOString().slice(0, 10)}
-            />
-          </div>
-          <div className="edit-input">
-            <label htmlFor="aboutme">About Me</label>
-            <textarea
+              />
+              </div>
+              <div className="edit-input">
+              <label htmlFor="aboutme">About Me</label>
+              <textarea
               id="aboutme"
               type="date"
               value={aboutMe}
               onChange={onChange}
               rows="5"
               cols="35"
-            ></textarea>
-          </div>
-          <button type="submit">Submit</button>
-        </form> */}
-      </main>
-    </div>
+              ></textarea>
+              </div>
+              <button type="submit">Submit</button>
+            </form> */}
+        </main>
+      </div>
+    </>
   );
 };
 export default EditProfilePage;
