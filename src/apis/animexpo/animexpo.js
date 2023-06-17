59f4cfc86ae3ac093dceac5c74b9c9c39e5d590c
Itamar <itamarshmaya@gmail.com>
2023-06-17 11:56:46 +0300

    fix

--- src/apis/animexpo/animexpo.js
+++ src/apis/animexpo/animexpo.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const animexpo = axios.create({
-//   baseURL: "https://animexposerver.onrender.com",
-// });
 const animexpo = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: "https://animexposerver.onrender.com",
 });
+// const animexpo = axios.create({
+//   baseURL: "http://localhost:5050",
+// });
 
 export default animexpo;
