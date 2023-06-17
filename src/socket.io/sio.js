cc34870ff20e803d9406ee1e6041500c90bab55f
Itamar <itamarshmaya@gmail.com>
2023-06-17 12:30:23 +0300

    socket.io reconnection fix

--- src/socket.io/sio.js
+++ src/socket.io/sio.js
@@ -2,6 +2,6 @@ import io from "socket.io-client";
 
 const URL = "https://animexposerver.onrender.com";
 // const URL = "http://localhost:5050";
-const socket = io(URL, { autoConnect: false });
+const socket = io(URL, { autoConnect: false, reconnection: false });
 
 export default socket;
