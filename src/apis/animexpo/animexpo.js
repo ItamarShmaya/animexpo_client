5945b09b94a03b192a5c1ba4cf53c9821ba54905
Itamar <itamarshmaya@gmail.com>
2023-06-19 14:02:00 +0300

    dev/prod env fix

--- src/apis/animexpo/animexpo.js
+++ src/apis/animexpo/animexpo.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://animexposerver.onrender.com"
-    : "http://localhost:5050";
+const baseURL = !process.env.NODE_ENV
+  ? "http://localhost:5050"
+  : "https://animexposerver.onrender.com";
 
 const animexpo = axios.create({ baseURL });
 
