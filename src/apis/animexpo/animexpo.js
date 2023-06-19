05da1fe2453f6f8845f5de9e412c58459936046f
Itamar <itamarshmaya@gmail.com>
2023-06-19 15:01:16 +0300

    dev/prod env fix

--- src/apis/animexpo/animexpo.js
+++ src/apis/animexpo/animexpo.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const baseURL = !process.env.NODE_ENV
-  ? "http://localhost:5050"
-  : "https://animexposerver.onrender.com";
-
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5050"
+    : "https://animexposerver.onrender.com";
+console.log(baseURL);
 const animexpo = axios.create({ baseURL });
 
 export default animexpo;
--- src/socket.io/sio.js
+++ src/socket.io/sio.js
@@ -1,9 +1,10 @@
 import io from "socket.io-client";
 
-const URL = !process.env.NODE_ENV
-  ? "http://localhost:5050"
-  : "https://animexposerver.onrender.com";
-
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5050"
+    : "https://animexposerver.onrender.com";
+console.log(URL);
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
